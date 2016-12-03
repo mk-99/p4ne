544eaa24af4ea1324a9bebdb0d5fe0c5ba72cc6b
mk-99 <mklochkov@gmail.com>
2016-12-03 23:32:26 +0300

    Seminar 1.3 added

--- Lab1.3/snmp.py
+++ Lab1.3/snmp.py
@@ -1,23 +1,37 @@
 #!/usr/local/bin/python3
 
-# Import some objects from library
+# Import some objects from 'pysnmp' library. 'hlapi' means high-level API
 from pysnmp.hlapi import *
 
-# Initialize accessory objects
+def print_snmp(g):
+    """Takes a generator object from pysnmp, prints snmp values"""
+    # Actual request performs here.
+    for snmp_result in g:
+        errorIndication, errorStatus, errorIndex, varBinds = snmp_result
+        if errorIndication:
+            print(errorIndication)
+        elif errorStatus:
+            print('%s at %s' % (errorStatus.prettyPrint(),
+                                errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        else:
+            for varBind in varBinds:
+                print(' = '.join([x.prettyPrint() for x in varBind]))
 
-engine = SnmpEngine()
-community_data = CommunityData('public')
-transfer_method = UdpTransportTarget(('10.31.70.107', 161))
-mib_object = ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+# g is a 'generator' object, it's elements returned are lists of [errorIndication, errorStatus, errorIndex, varBinds]
+g = getCmd(SnmpEngine(),
+           CommunityData('public', mpModel=0),
+           UdpTransportTarget(('10.31.70.107', 161)),
+           ContextData(),
+           ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
 
-g = getCmd(engine, community_data, transfer_method, ContextData(), mib_object) # g is a 'generator'
+# Now it is time to print results
+print_snmp(g)
 
-errorIndication, errorStatus, errorIndex, varBinds = next(g) # Actual request performs here. Multiple assignment example
+n = nextCmd(SnmpEngine(),
+           CommunityData('public', mpModel=0),
+           UdpTransportTarget(('10.31.70.107', 161)),
+           ContextData(),
+           ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
+           lexicographicMode=False)
 
-for varBind in varBinds:
-    print(' = '.join([x.prettyPrint() for x in varBind]))
-
-mib_object = ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
-g = getCmd(engine, community_data, transfer_method, ContextData(), mib_object) # g is a 'generator'
-
-errorIndication, errorStatus, errorIndex, varBinds = next(g) # Actual request performs here. Multiple assignment example
+print_snmp(n)
---
+++ Sem1.3/fib_yield.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+
+def fib(n):
+    a, b = 1, 1
+    step = 0
+    while step < n:
+        yield a
+        b, a = a + b, b
+        step += 1
+
+def fibr(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fibr(n - 1) + fibr(n - 2)
+
+for i in fib(100):
+    print(i)
+
+for i in range(100):
+    print(fibr(i))
